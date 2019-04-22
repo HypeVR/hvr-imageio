bea0f5445f11ab3ebe676e6c90c7409a60ce8de6
Achal Shah <achalshah20@gmail.com>
2019-04-22 16:06:05 -0700

    Unit test for image io

---
+++ unit-test/ImageIO/include/ImageIOTest.h
@@ -0,0 +1,20 @@
+// Copyright 2019 Achal Shah
+
+#ifndef UNIT_TEST_IMAGEIO_INCLUDE_IMAGEIOTEST_H_
+#define UNIT_TEST_IMAGEIO_INCLUDE_IMAGEIOTEST_H_
+
+#include <string>
+
+#include "gtest/gtest.h"
+
+class ImageIOTest : public ::testing::Test
+{
+ protected:
+  ImageIOTest();
+  ~ImageIOTest() override;
+
+  void SetUp() override;
+  void TearDown() override;
+};
+
+#endif  // UNIT_TEST_IMAGEIO_INCLUDE_IMAGEIOTEST_H_
---
+++ unit-test/ImageIO/src/ImageIOTest.cpp
@@ -0,0 +1,95 @@
+// Copyright 2019 Achal Shah
+
+#include "ImageIOTest.h"
+
+HVR_WINDOWS_DISABLE_ALL_WARNING
+#include "glog/logging.h"
+#include "opencv2/opencv.hpp"
+HVR_WINDOWS_ENABLE_ALL_WARNING
+
+#include "Hvr/ImageIO/ImageIO.h"
+
+extern std::string exe_path;
+
+ImageIOTest::ImageIOTest() = default;
+
+ImageIOTest::~ImageIOTest() = default;
+
+void ImageIOTest::SetUp()
+{
+}
+
+void ImageIOTest::TearDown()
+{
+}
+
+TEST_F(ImageIOTest, ExivImgReadTest)
+{
+  std::map<std::string, std::string> exiv_data;
+  cv::Mat img = hvr::ImageIO::ReadImageExiv(
+      exe_path + "/data/ImageIO/face_viewlbl_exiv.tif", 1, exiv_data);
+
+  for (auto &it : exiv_data)
+  {
+    if (it.first == "Xmp.hvr.frame_num")
+    {
+      ASSERT_EQ(it.second, "1");
+    }
+    else if (it.first == "Xmp.hvr.frontface_pod_num")
+    {
+      ASSERT_EQ(it.second, "POD02");
+    }
+  }
+}
+
+TEST_F(ImageIOTest, ExivReadWriteTest)
+{
+  cv::Mat img = cv::imread(exe_path + "/data/ImageIO/random_img.tif");
+
+  std::map<std::string, std::string> exiv_data;
+  exiv_data.insert(
+      std::pair<std::string, std::string>("Xmp.hvr.key1", "value1"));
+  exiv_data.insert(
+      std::pair<std::string, std::string>("Xmp.hvr.key2", "value2"));
+
+  hvr::ImageIO::WriteImageExiv("temp_out.tiff", img, exiv_data);
+
+  // Reading file
+  std::map<std::string, std::string> out_exiv_data;
+
+  cv::Mat temp_out =
+      hvr::ImageIO::ReadImageExiv("temp_out.tiff", 1, out_exiv_data);
+
+  for (auto &it : out_exiv_data)
+  {
+    if (it.first == "Xmp.hvr.key1")
+    {
+      ASSERT_EQ(it.second, "value1");
+    }
+    else if (it.first == "Xmp.hvr.key2")
+    {
+      ASSERT_EQ(it.second, "value2");
+    }
+  }
+}
+
+TEST_F(ImageIOTest, ExivReadTest)
+{
+  std::map<std::string, std::string> exiv_data;
+  bool status = hvr::ImageIO::ReadExiv(
+      exe_path + "/data/ImageIO/face_viewlbl_exiv.tif", exiv_data);
+
+  ASSERT_EQ(status, true);
+
+  for (auto &it : exiv_data)
+  {
+    if (it.first == "Xmp.hvr.frame_num")
+    {
+      ASSERT_EQ(it.second, "1");
+    }
+    else if (it.first == "Xmp.hvr.frontface_pod_num")
+    {
+      ASSERT_EQ(it.second, "POD02");
+    }
+  }
+}
